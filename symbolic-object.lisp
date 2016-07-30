a151fea28b46acc2a08d3a6b72356375abb58418
yokota yuki <y2q.actionman@gmail.com>
2016-07-30 11:22:35 +0900

    changes field maker

--- symbolic-object.lisp
+++ symbolic-object.lisp
@@ -55,53 +55,61 @@
   
 
 ;; class
-(defmacro define-symbolic-object-class (name super-class-names fields methods)
-  `(loop for class-name in ',super-class-names
-      append (get class-name :symbolic-class-fields) into this-class-fields
-      append (get class-name :symbolic-class-methods) into this-class-methods
-      finally
-	(setf (get ',name :symbolic-class-fields) (append this-class-fields ',fields)
-	      (get ',name :symbolic-class-methods) (append this-class-methods ',methods))))
+(defun find-symbolic-object-namespace (s-object)
+  (let ((namespace-package-name (format nil "~S" s-object)))
+    (or (find-package namespace-package-name)
+	(make-package namespace-package-name :use nil))))
 
-(defun add-symbolic-object-fields (s-object fields)
-  (let* ((namespace-package-name (format nil "~S" s-object))
-	 (namespace (or (find-package namespace-package-name)
-			(make-package namespace-package-name :use nil)))
-	 (field-alist nil))
-    ;; * field 生成
-    ;; package を新しい名前空間にする。
+(defmacro define-symbolic-object-class (name super-class-names fields methods)
+  (let ((field-specs nil))
+    ;; makes some global defs
     (loop for field-def in fields
        as field-name = (first field-def)
+       as reader-name = (intern (format nil "~S-reader" field-name))
+       as writer-name = (intern (format nil "~S-writer" field-name))
+       ;; global defs
+       unless (fboundp field-name)
+       do (add-global-function-to-symbolic-object-method field-name reader-name)
+       unless (fboundp `(setf ,field-name))
+       do (add-global-function-to-symbolic-object-method `(setf ,field-name) writer-name)
+       ;; reader/writer を作る関数を作る
+       do (let ((reader-name reader-name)
+		(writer-name writer-name))
+	    (flet ((make-reader/writer (s-object sym)
+		     (add-symbolic-object-method s-object reader-name
+						 (lambda () (symbol-value sym)))
+		     (add-symbolic-object-method s-object writer-name
+						 (lambda (val) (setf (symbol-value sym) val)))))
+	      (push `(,field-name ,#'make-reader/writer ,@(rest field-def)) field-specs)))
+       ;; 
+       finally (setf field-specs (nreverse field-specs)))
+    `(loop for class-name in ',super-class-names
+	append (get class-name :symbolic-class-field-specs) into this-class-field-specs
+	append (get class-name :symbolic-class-methods) into this-class-methods
+	finally
+	  (setf (get ',name :symbolic-class-field-specs) (append this-class-field-specs ',field-specs)
+		(get ',name :symbolic-class-methods) (append this-class-methods ',methods)))))
+
+(defun apply-symbolic-object-class (s-object symbolic-class-name)
+  (let* ((field-specs (get symbolic-class-name :symbolic-class-field-specs))
+	 (methods (get symbolic-class-name :symbolic-class-methods))
+	 (namespace-package-name (format nil "~S" s-object))
+	 (namespace (or (find-package namespace-package-name)
+			(make-package namespace-package-name :use nil))))
+    ;; * field 生成
+    (loop for (field-name reader/writer-maker . field-inits) in field-specs
        as sym = (intern field-name namespace)
        ;; initialize
-       when (>= (length field-def) 2)	; TODO: use length= in alexandria
-       do (setf (symbol-value sym) (second field-def))
+       when field-inits
+       do (setf (symbol-value sym) (first field-inits))
        ;; reader/writer
-       do (let ((reader-name (intern (format nil "~S-reader" field-name))))
-	    (add-symbolic-object-method s-object reader-name
-					(let ((sym sym))
-					  (lambda () (symbol-value sym))))
-	    (add-global-function-to-symbolic-object-method field-name reader-name))
-       do (let ((writer-name (intern (format nil "~S-writer" field-name))))
-	    (add-symbolic-object-method s-object writer-name
-					(let ((sym sym))
-					  (lambda (val) (setf (symbol-value sym) val))))
-	    (add-global-function-to-symbolic-object-method `(setf ,field-name) writer-name))
-       ;; make renaming entry
-       do (setf field-alist (acons field-name sym field-alist))))
-  s-object)
-
-(defmacro apply-symbolic-object-class (s-object symbolic-class-name)
-  (let* ((fields (get symbolic-class-name :symbolic-class-fields))
-	 (methods (get symbolic-class-name :symbolic-class-methods)))
-    `(progn
-       (add-symbolic-object-fields ,s-object ',fields)
+       do (funcall reader/writer-maker s-object sym))
     ;; ;; * method 生成
     ;; (loop for (method-name method-lambda-list . method-body) in methods ; TODO: use parse-lambda in alexandria
     ;;    do (add-symbolic-object-method s-object method-name
     ;; 				      `(lambda (,@method-lambda-list) ,@method-body))
     ;; 	 (add-global-function-to-symbolic-object-method method-name)))
-       ,s-object)))
+    s-object))
 
 (define-symbolic-object-class test-class ()
   ((field-a 1)
@@ -117,4 +125,4 @@
   ((class-method-all-sum (&rest args)
 			 (apply #'+ field-a field-b field-c args))))
 
-(apply-symbolic-object-class 'test-obj test-class)
+(apply-symbolic-object-class 'test-obj 'test-class)
