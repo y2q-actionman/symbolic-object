2e403ba9715f3d4f496211fb8a8dfd68a5570b33
yokota yuki <y2q.actionman@gmail.com>
2016-07-30 13:01:06 +0900

    added spec merging

--- symbolic-object.lisp
+++ symbolic-object.lisp
@@ -55,15 +55,24 @@
   
 
 ;; class
-(defun find-symbolic-object-namespace (s-object)
-  (let ((namespace-package-name (format nil "~S" s-object)))
-    (or (find-package namespace-package-name)
-	(make-package namespace-package-name :use nil))))
+(defun add-spec-into-spec-list (new-spec spec-list)
+  (loop with ret = nil
+     for spec in spec-list
+     unless (eq (first spec) (first new-spec))
+     do (push spec ret)
+     finally
+       (push new-spec ret)
+       (return ret)))
+
+(defun merge-spec-list (spec-list new-spec-list)
+  (loop for s in new-spec-list
+     do (setf spec-list (add-spec-into-spec-list s spec-list)))
+  spec-list)
 
 (defmacro define-symbolic-object-class (name super-class-names fields methods)
   (let ((field-specs nil)
-	(field-names nil)
-	(method-specs nil))
+	(method-specs nil)
+	(field-names nil))
     ;; makes some global defs
     (loop for field-def in fields
        as field-name = (first field-def)
@@ -76,46 +85,49 @@
        do (add-global-function-to-symbolic-object-method `(setf ,field-name) writer-name)
        ;; spec 回収
        do (push `(,field-name ,reader-name ,writer-name ,@(rest field-def)) field-specs)
-	 (push field-name field-names)
        ;; 
-       finally (setf field-specs (nreverse field-specs)
-		     field-names (nreverse field-names)))
-    `(loop for class-name in ',super-class-names
-	append (get class-name :symbolic-class-field-specs) into this-class-field-specs
-	append (get class-name :symbolic-class-method-specs) into this-class-method-specs
-	finally
-	  (flet (,@(loop for (method-name method-lambda-list . method-body) in methods
-		      as method-maker-name = (gensym (format nil "method-maker-~A" method-name))
-		      ;; global defs
-		      unless (fboundp method-name)
-		      do (add-global-function-to-symbolic-object-method method-name)
-		      ;; spec 回収
-		      do (push `(list ',method-name #',method-maker-name) method-specs)
-		      ;; flet form
-		      collect `(,method-maker-name (,@field-names)
-						   (declare (ignorable ,@field-names))
-						   (lambda (,@method-lambda-list)
-						     (let (,@(loop for f in field-names
-								collect `(,f (symbol-value ,f))))
-						       (declare (ignorable ,@field-names))
-						       ,@method-body)))
-		      into flet-defs
-		      ;;
-		      finally
-			(setf method-specs (nreverse method-specs))
-			(return flet-defs)))
-	    (setf (get ',name :symbolic-class-field-specs)
-		  (append this-class-field-specs ',field-specs)
-		  (get ',name :symbolic-class-method-specs)
-		  (append this-class-method-specs (list ,@method-specs)))))))
-;; TODO: fields merging
+       finally (setf field-specs (nreverse field-specs)))
+    ;; merge field specs
+    (loop with this-class-field-specs = nil
+       for class-name in super-class-names
+       do (setf this-class-field-specs (merge-spec-list this-class-field-specs
+							(get class-name :symbolic-class-field-specs)))
+       finally
+	 (setf this-class-field-specs
+	       (merge-spec-list this-class-field-specs field-specs))
+	 (setf (get name :symbolic-class-field-specs)
+	       (merge-spec-list this-class-field-specs field-specs))
+	 (setf field-names (mapcar #'car this-class-field-specs)))
+    ;; generate method-making functions
+    `(flet (,@(loop for (method-name method-lambda-list . method-body) in methods
+		 as method-maker-name = (gensym (format nil "method-maker-~A" method-name))
+		 ;; global defs
+		 unless (fboundp method-name)
+		 do (add-global-function-to-symbolic-object-method method-name)
+		 ;; spec 回収
+		 do (push `(list ',method-name #',method-maker-name) method-specs)
+		 ;; flet form
+		 collect `(,method-maker-name (,@field-names)
+					      (declare (type symbol ,@field-names))
+					      (lambda (,@method-lambda-list)
+						(let (,@(loop for f in field-names
+							   collect `(,f (symbol-value ,f))))
+						  (declare (ignorable ,@field-names))
+						  ,@method-body)))
+		 into flet-defs
+		 ;;
+		 finally
+		   (setf method-specs (nreverse method-specs))
+		   (return flet-defs)))
+       ;; merge method specs
+       (loop with this-class-method-specs = nil
+	  for class-name in ',super-class-names
+	  do (setf this-class-method-specs (merge-spec-list this-class-method-specs
+							    (get class-name :symbolic-class-method-specs)))
+	  finally
+	    (setf (get ',name :symbolic-class-method-specs)
+		  (merge-spec-list this-class-method-specs (list ,@method-specs)))))))
 ;; TODO: use parse-lambda in alexandria
-
-(defun make-symbolic-object-field-reader/writer (s-object field-symbol reader-name writer-name)
-  (add-symbolic-object-method s-object reader-name
-			      (lambda () (symbol-value field-symbol)))
-  (add-symbolic-object-method s-object writer-name
-			      (lambda (val) (setf (symbol-value field-symbol) val))))
 
 (defun apply-symbolic-object-class (s-object symbolic-class-name)
   (let* ((field-specs (get symbolic-class-name :symbolic-class-field-specs))
@@ -131,7 +143,12 @@
        when field-inits
        do (setf (symbol-value sym) (first field-inits))
        ;; reader/writer
-       do (make-symbolic-object-field-reader/writer s-object sym reader-name writer-name)
+       do (add-symbolic-object-method s-object reader-name
+				      (let ((field-symbol sym))
+					(lambda () (symbol-value field-symbol))))
+	 (add-symbolic-object-method s-object writer-name
+				     (let ((field-symbol sym))
+					  (lambda (val) (setf (symbol-value field-symbol) val))))
        ;;
        collect sym into field-syms
        finally
@@ -157,3 +174,12 @@
 			 (apply #'+ field-a field-b field-c args))))
 
 (apply-symbolic-object-class 'test-obj 'test-class)
+(apply-symbolic-object-class 'test-obj-2 'test-class-2)
+
+(call-symbolic-method 'test-obj 'class-method-hello)
+(call-symbolic-method 'test-obj 'class-method-describe)
+
+(call-symbolic-method 'test-obj-2 'class-method-hello)
+(call-symbolic-method 'test-obj-2 'class-method-describe)
+(call-symbolic-method 'test-obj-2 'class-method-all-sum)
+(call-symbolic-method 'test-obj-2 'class-method-all-sum 1000)
